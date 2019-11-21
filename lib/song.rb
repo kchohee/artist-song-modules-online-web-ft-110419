588a9fb71f56950a5dc2b50baf34bc8a35537653
Marie (Cho) Hughes <kchohee0630@gmail.com>
2019-11-21 06:37:46 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,6 +2,7 @@ require 'pry'
 # require_relative '../lib/concerns/memorable'
 class Song
   extend Memorable::ClassMethods
+  include Memorable::InstanceMethods
   attr_accessor :name
   attr_reader :artist
   @@songs = []
