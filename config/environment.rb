1dfd98d921e65e2b611d3f02a6ced1418fc59452
Marie (Cho) Hughes <kchohee0630@gmail.com>
2019-11-21 06:30:12 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -4,7 +4,8 @@ require_relative '../lib/concerns/findable'
 # require_relative '../lib/concerns/memorable'
 # require_relative '../lib/concerns/paramable'
 
-
+puts Song.Memorable
+puts Artist.Memorable
 
 require_relative '../lib/artist.rb'
 require_relative '../lib/song.rb'
