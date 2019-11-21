cca9817f89f89b55cbdc92ae79fd027192ec2e3a
Marie (Cho) Hughes <kchohee0630@gmail.com>
2019-11-21 06:45:40 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -9,9 +9,9 @@ class Artist
     @@artists << self
     @songs = []
   end
-  def self.find_by_name(name)
-    @@artists.detect{|a| a.name == name}
-  end
+  # def self.find_by_name(name)
+  #   @@artists.detect{|a| a.name == name}
+  # end
   def self.all
     @@artists
   end
