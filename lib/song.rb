c5c2b46ddaec4aa7daeff79668d1967be1e5743f
Marie (Cho) Hughes <kchohee0630@gmail.com>
2019-11-21 06:45:38 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,9 +9,9 @@ class Song
   def initialize
     @@songs << self
   end
-  def self.find_by_name(name)
-    @@songs.detect{|a| a.name == name}
-  end
+  # def self.find_by_name(name)
+  #   @@songs.detect{|a| a.name == name}
+  # end
   def self.all
     @@songs
   end
