c9fde9629124d25e7c47ef02c63283feabaa33b3
Marie (Cho) Hughes <kchohee0630@gmail.com>
2019-11-21 06:37:49 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -2,6 +2,7 @@ require 'pry'
 # require_relative '../lib/concerns/memorable'
 class Artist
   extend Memorable::ClassMethods
+  include Memorable::InstanceMethods
   attr_accessor :name
   attr_reader :songs
   @@artists = []
